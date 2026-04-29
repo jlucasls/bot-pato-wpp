17edf07fa8f44d85768fffdfdf5d1752f3337b72
JOAO LUCAS DA LUZ LUQUINI SILVA <021.885219@universidadesenaicimatec.edu.br>
2026-04-29 18:19:52 -0300

    atualização 2.0

--- services/menu.js
+++ services/menu.js
@@ -1,15 +1,12 @@
 function getMenu() {
   return `
-👋 Olá! Eu sou o bot do Clube de Programação 🐤
+🦆 QUACK! Eu sou o bot do Clube de Programação 🐤
 
 Escolha uma opção:
 
 1 - 📢 Pato News
 2 - 📺 PatoTv
 3 - 🌐 Site do clube
-4 - 🧮 Ferramentas
-5 - 📅 Eventos
-6 - ❓ Ajuda
 `;
 }
 
